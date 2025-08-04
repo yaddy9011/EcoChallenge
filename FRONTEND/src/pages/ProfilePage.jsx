8763da86c01c46dfdee80e7c6ee2cac4d79044f9
Oriak <diegooriak3@outlook.com>
2025-08-04 13:53:57 -0600

    Diseño de Perfil en Progreso

---
+++ FRONTEND/src/pages/ProfilePage.jsx
@@ -0,0 +1,21 @@
+
+
+const ProfilePage= () =>{
+    const consulta=()=>{
+
+    }
+    return(
+        <>
+            <h1>Perfil de Jugador</h1>
+            <div className="ProfileContainer">
+                <img></img>
+                <h2>Nombre de Jugador</h2>
+                <div className="ProfileLevel">Nivel de Jugador</div>
+                <div className="GoalsHit">
+                    <h2>Hitos</h2>
+                    <p>Puntos Acumulados y Retos Cumplidos</p>
+                </div>
+            </div>
+        </>
+    )
+}
