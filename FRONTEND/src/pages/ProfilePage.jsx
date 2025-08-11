065442a05ad06f2f48aab21eeb1edcbb74418a01
miguel550151 <miguel.hm@ucad.edu.mx>
2025-08-11 13:29:30 -0600

    cambio 1

--- FRONTEND/src/pages/ProfilePage.jsx
+++ FRONTEND/src/pages/ProfilePage.jsx
@@ -9,11 +9,20 @@ export default function  ProfilePage() {
             <h1>Perfil de Jugador</h1>
             <div className="ProfileContainer">
                 <img></img>
-                <h2>Nombre de Jugador</h2>
-                <div className="ProfileLevel">Nivel de Jugador</div>
-                <div className="GoalsHit">
-                    <h2>Hitos</h2>
-                    <p>Puntos Acumulados y Retos Cumplidos</p>
+
+                <div className="Nombre">
+                <h2>Nombre de Jugador:<label id="nombreUsuario"></label></h2>
+                </div>
+
+                <div className="Nivel">
+                    <h2>Nivel de jugador:<label id="nivelUsuario"></label></h2>
+                </div>
+
+                <div className="Logros">
+                    <h2>Logros</h2>
+                    <div id="Logro1"></div>
+                    <div id="Logro2"></div>
+                    <div id="Logro3"></div>
                 </div>
             </div>
         </>
